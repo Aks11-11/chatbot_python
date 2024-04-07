ceed94f50639526efc30f56d68fa42bd8249e21d
Aks11-11 <aksshatvashisht@gmail.com>
2024-04-07 14:54:23 +0530

    /

---
+++ chatbot/2_chatbot.py
@@ -0,0 +1,16 @@
+# Create the vectorizer and classifier
+vectorizer = TfidfVectorizer()
+clf = LogisticRegression(random_state=0, max_iter=10000)
+
+# Preprocess the data
+tags = []
+patterns = []
+for intent in intents:
+    for pattern in intent['patterns']:
+        tags.append(intent['tag'])
+        patterns.append(pattern)
+
+# training the model
+x = vectorizer.fit_transform(patterns)
+y = tags
+clf.fit(x, y)
---
+++ chatbot/3_chatbot.py
@@ -0,0 +1,7 @@
+def chatbot(input_text):
+    input_text = vectorizer.transform([input_text])
+    tag = clf.predict(input_text)[0]
+    for intent in intents:
+        if intent['tag'] == tag:
+            response = random.choice(intent['responses'])
+            return response
