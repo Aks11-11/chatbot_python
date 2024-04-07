132bd98fd97bc311f6d43827dc5d12aa2a211fef
Aks11-11 <aksshatvashisht@gmail.com>
2024-04-07 14:47:30 +0530

    /

--- main2.py
+++ main2.py
@@ -1,6 +1,5 @@
 #default message at the start of chat
 print("Hi, I'm Akshat and I like to chat\nPlease type lowercase English language to start a conversation. Type quit to leave ")
-#Create Chat Bot
 chat = Chat(pairs, reflections)
 #Start conversation
 chat.converse()
